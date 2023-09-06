4da680ba6c3804017294d607eeaec626e935bbeb
Eugene Cross <eugene.cross@gmail.com>
2023-09-06 10:20:01 +0100

    Core data DataController added

---
+++ FriendFace/DataController.swift
@@ -0,0 +1,24 @@
+//
+//  DataController.swift
+//  FriendFace
+//
+//  Created by Eugene on 06/09/2023.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "FriendFace")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("FriendFace failed to load: \(error.localizedDescription)")
+                return
+            }
+            
+            self.container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyObjectTrump
+        }
+    }
+}
