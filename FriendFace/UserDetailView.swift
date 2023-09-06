1cbcc6d5c1d690b69cd87f501a25cc7ab3929179
Eugene Cross <eugene.cross@gmail.com>
2023-09-06 11:43:25 +0100

    Remove UserDetailView preview and dummy data

--- FriendFace/UserDetailView.swift
+++ FriendFace/UserDetailView.swift
@@ -82,20 +82,4 @@ struct UserDetailView: View {
         )
     }
 }
-//
-//struct UserDetailView_Previews: PreviewProvider {
-//
-//    static let user = User(id: "a9c659b8-979e-4603-a418-9f5286449fbb", isActive: true, name:  "Mclean Alford", age: 29, company: "Kiggle", email: "mcleanalford@kiggle.com", address: "312 Danforth Street, Elizaville, Vermont, 5877", about: "Ut ut dolore quis excepteur consequat adipisicing et sint nostrud velit consequat. In esse officia deserunt tempor. Pariatur in dolor aliqua aute exercitation est. Eiusmod et sit est reprehenderit consectetur sint aliquip ex officia irure veniam voluptate irure duis. Sit enim nulla ea elit aliquip laborum deserunt anim nisi eu.", registered: Date.now, friends: [
-//        Friend(id: "19ea3704-c0f6-41d5-a08f-00a09e1da82a", name: "Rhodes Carr"),
-//        Friend(id: "22934d37-6bbd-4023-b99b-88819eeee0da", name: "Joanna Hurst"),
-//        Friend(id: "63caacb0-7312-41ca-ad9b-33cb93e6c85d", name: "Kristine Kinney"),
-//        Friend(id: "1744a058-d78a-414f-ab8b-8b02432703d7",name: "Manning Richard"),
-//        Friend(id: "4d15d138-f3c4-460e-a03f-e1235f2df62b",name: "Tucker Alexander"),
-//        Friend(id:"d8cc81db-9e4a-4ac1-afba-d287f5142cb6",name: "Tammie Prince")
-//    ])
-//
-//    static var previews: some View {
-//        UserDetailView(user: user)
-//    }
-//}
 
